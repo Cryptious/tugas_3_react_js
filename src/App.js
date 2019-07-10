acc0ace7063668ddbe778909e92bafde08ee328e
Martino Wagey <martinowagey@gmail.com>
2019-07-10 14:20:02 +0700

    add tugas 3 React JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Footer from "./Page/Footer";
+import MenuMakanan from "./Page/MenuMakanan";
+import Header from "./Page/Header";
+import MenuKontak from "./Page/MenuKontak";
+import MenuTentangKami from "./Page/MenuTentangKami";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Header />
+        <br />
+        <br />
+        <MenuMakanan />
+        <MenuTentangKami />
+        <MenuKontak />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
